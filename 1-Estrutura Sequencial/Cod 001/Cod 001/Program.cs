8c5b82db848edabb1fbf9f9860250dfbe92cd22e
VMBilate <vmbilate@gmail.com>
2025-02-12 11:22:51 -0300

    Add files via upload

---
+++ 1-Estrutura Sequencial/Cod 001/Cod 001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, Soma;
+
+            Console.Write("Digite o primeiro número inteiro: ");
+            while (!int.TryParse(Console.ReadLine(), out A))
+            {
+                Console.Write("Entrada inválida! Digite um número inteiro: ");
+            }
+
+            Console.Write("Digite o segundo número inteiro: ");
+            while (!int.TryParse(Console.ReadLine(), out B))
+            {
+                Console.Write("Entrada inválida! Digite um número inteiro: ");
+            }
+
+            Soma = A + B;
+
+            Console.WriteLine($"Soma = {Soma}");
+        }
+    }
+}
---
+++ 1-Estrutura Sequencial/Cod 004/Cod 004/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero, horas;
+            double valorHora, salario;
+
+            Console.Write("Digite o número do funcionário: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de horas trabalhadas: ");
+            horas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da hora trabalhada: ");
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valorHora * horas;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
