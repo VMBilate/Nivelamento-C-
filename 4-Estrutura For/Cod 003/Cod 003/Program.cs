8de141b6715248a95bbf12c4baf3ecf00260172d
VMBilate <vmbilate@gmail.com>
2025-02-12 11:23:32 -0300

    Add files via upload

---
+++ 4-Estrutura For/Cod 003/Cod 003/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de casos de teste: ");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.WriteLine("Número inválido. Encerraando o programa.");
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Digite os três números para o caso {i + 1}: ");
+                string[] line = Console.ReadLine().Split(' ');
+
+                if (line.Length != 3 ||
+                    !double.TryParse(line[0], NumberStyles.Float, CultureInfo.InvariantCulture, out double a) ||
+                    !double.TryParse(line[1], NumberStyles.Float, CultureInfo.InvariantCulture, out double b) ||
+                    !double.TryParse(line[2], NumberStyles.Float, CultureInfo.InvariantCulture, out double c))
+                {
+                    Console.WriteLine("Entrada inválida. Certifique-se de inserir três números separados por espaço.");
+                    i--;
+                    continue;
+                }
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+                Console.WriteLine($"Média ponderada: {media.ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
