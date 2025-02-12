a381ff910a95cb0aa4c5b8accadd9d911243145f
VMBilate <vmbilate@gmail.com>
2025-02-12 11:23:48 -0300

    Add files via upload

---
+++ 5-Classes,Atributos,Métodos,Membros Estáticos/Cod 003/Cod 003/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retangulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
