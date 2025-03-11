9b5a82689814189e1e0aead83904edc2ea20735f
VMBilate <vmbilate@gmail.com>
2025-03-11 11:11:38 -0300

    Add files via upload

---
+++ 13-Generics ser dictionary/Cod 002/Cod 002/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> CourseA = new HashSet<int>();
+            HashSet<int> CourseB = new HashSet<int>();
+            HashSet<int> CourseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                CourseA.Add(cod);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                CourseB.Add(cod);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                CourseC.Add(cod);
+            }
+
+            HashSet<int> all = new HashSet<int>(CourseA);
+            all.UnionWith(CourseB);
+            all.UnionWith(CourseC);
+            Console.WriteLine("Total students: " + all.Count);
+        }
+    }
+}
