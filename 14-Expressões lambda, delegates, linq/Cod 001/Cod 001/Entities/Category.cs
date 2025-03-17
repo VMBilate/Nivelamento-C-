c7bfbb18d529bbd6c2a3a52d84c171fe47070c1a
VMBilate <vmbilate@gmail.com>
2025-03-17 10:24:51 -0300

    Add files via upload

---
+++ 14-Expressões lambda, delegates, linq/Cod 001/Cod 001/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Course.Exercicio1
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ 14-Expressões lambda, delegates, linq/Cod 003/Cod 003/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Course.Exercicio3
+{
+    class Employee
+    {
+
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public Double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
